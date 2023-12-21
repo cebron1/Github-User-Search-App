e47efbdbb60ac5184e5e69706c3deddd859680b6
Christopher Ebron <cebron88@gmail.com>
2023-12-21 16:31:44 -0500

    form fix

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-appUrl = 'https://api.github.com'
 
-fetch(appUrl)
-  .then(res => res.json())
-  .then(data => console.log(data))
+username = document.getElementById("form");
+baseApiUrl = 'https://api.github.com/users';
+
+
+fetch(apiUrl)
+  apiUrl = `${baseApiUrl}/${username}`
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok or invalid username');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Data from the API:', data);
+    // Perform operations with the received data
+  })
+  .catch(error => {
+    console.error('There was a problem fetching data:', error);
+  });
