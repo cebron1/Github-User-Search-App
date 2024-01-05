442f16cea20830bcaef5847117e2f1a6517cdd1f
Christopher Ebron <cebron88@gmail.com>
2024-01-04 20:54:10 -0500

    Consumed form input

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 
-username = document.getElementById("form");
-baseApiUrl = 'https://api.github.com/users';
+const search = document.getElementById("searchButton");
+let userProfileName;
+
+//Add event listener for search bar
+search.addEventListener('click', function(event) {
+  //Prevent default behavior
+  event.preventDefault();
+  const userProfileName = document.getElementById('userInput').value;
+  console.log(userProfileName)       //TODO Erase
+
+});
 
 
-fetch(apiUrl)
-  apiUrl = `${baseApiUrl}/${username}`
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok or invalid username');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log('Data from the API:', data);
-    // Perform operations with the received data
-  })
-  .catch(error => {
-    console.error('There was a problem fetching data:', error);
-  });
+
+
+
+
+
+
+
+
+
+
+// queryUsername = 'cebron1'   
+// baseApiUrl = 'https://api.github.com/users';
+// apiUrl = `${baseApiUrl}/${queryUsername}`
+
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error ('Network response was not ok or invalid username');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     displayAvatar(data.avatar_url);
+    
+//     // Perform operations with the received data
+//   })
+//   .catch(error => {
+//     console.error('There was a problem fetching data:', error);
+//   });
+  
+
+
+//   // Display info in HTML
+// function displayAvatar(avatarUrl){
+//   dataContainer = document.getElementById('avatar');
+//   dataContainer.src = avatarUrl;
+
+// }
+// //
